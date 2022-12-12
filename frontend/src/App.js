92a68eb6a9426f77e19955f94d1e1875f092c001
saimon hossain <saimonhossen05@saimons-MacBook-Air.local>
2022-12-12 23:34:37 +0600

    Notfound page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { Route , Routes} from "react-router-dom";
+import { Link, Route , Routes} from "react-router-dom";
+import PageNotFound from "./errorhandle/PageNotFound";
 import Crashcourses from "./home/Crashcourses";
 import Statussection from "./home/Statussection";
 import Login from "./User/Login";
@@ -6,6 +7,19 @@ import Login from "./User/Login";
 function App() {
   return (
     <div className="App mx-10">
+      <div className="py-2 grid grid-cols-2 gap-10">
+        <div>
+           <p className="text-4xl font-bold">Lear<span className=" text-five">ner</span></p>
+        </div>
+        <div className="flex  space-x-10 flex-row justify-end items-center text-xl">
+            <div>
+               <Link to="/">Home</Link>
+            </div>
+            <div>
+               <Link to="/login">Login</Link>
+            </div>
+        </div>
+      </div>
       <Routes>
         <Route
           path="/"
@@ -17,6 +31,7 @@ function App() {
           }
         />
         <Route path="/login" element={<Login/>} />
+        <Route path="*" element={<PageNotFound/>}/>
       </Routes>
     </div>
   );
