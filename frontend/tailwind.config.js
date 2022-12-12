c4ea0d0c08d882e65e86a51c9730173921c9f584
saimon hossain <saimonhossen05@saimons-MacBook-Air.local>
2022-12-12 21:39:50 +0600

    status section added

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'one': '#E3FDF2',
+      'two': '#F9F7E6',
+      'three': '#FFF1F2',
+      'four': '#EAE9F8',
+    },
+  },
+  plugins: [],
+}
