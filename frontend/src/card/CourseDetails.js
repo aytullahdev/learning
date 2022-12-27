151aa9df15cf9b99a7a8e88c881469160eb6ae10
ArthiDa <arthida150@gmail.com>
2022-12-28 01:54:33 +0600

    description add

--- frontend/src/card/CourseDetails.js
+++ frontend/src/card/CourseDetails.js
@@ -19,11 +19,12 @@ const CourseDetails = () => {
         <div className='my-5'>
             {courseData && 
                 <div className=' grid lg:grid-cols-2 items-center gap-10'>
-
                     <div>
                         <h1 className='pt-10 pb-5 text-xl font-semibold'>{courseData.tittle}</h1>
                         <p className='pb-10'>
-                        দৈনন্দিন জীবনে Spoken English-এ পারদর্শী ও আত্মবিশ্বাসী হয়ে উঠুন। ইংরেজি বলায় দক্ষ হয়ে উঠতে এনরোল করুন “ঘরে বসে Spoken English” কোর্সে।
+                        {
+                            courseData.description ? courseData.description : courseData.tittle+' only at '+courseData.price+'৳'
+                        }
                         </p>
                         <h1 className='text-xl font-bold py-5'>কোর্স ইন্সট্রাক্টর</h1>
                         <div className='flex shadow rounded p-5 '>
@@ -44,7 +45,7 @@ const CourseDetails = () => {
                                  <img className='mx-auto ' src={courseData.img} alt="" />
                             </div>
                             <div>
-                                <h1 className='text-2xl font-semibold py-10 px-10'>{courseData.price} $</h1>
+                                <h1 className='text-2xl font-semibold py-10 px-10'>{courseData.price} ৳</h1>
                             </div>
                             <div className='px-5'>
                                 <button className='w-full text-[white] text-xl mx-auto block py-2 mb-5 bg-five  hover:bg-[green] rounded'>কোর্সটি কিনুন</button>
