7a9a4c1c61eabda5babff045e4895ac2c75a3155
ArthiDa <arthida150@gmail.com>
2022-12-28 17:08:36 +0600

    profile enrollment done

--- frontend/src/User/EnrolCourses.js
+++ frontend/src/User/EnrolCourses.js
@@ -1,45 +1,55 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { ThemeContext } from '../App';
-import axios from 'axios';
-import SingleEnrollCourse from './SingleEnrollCourse';
+import React, { useContext, useEffect, useState } from "react";
+import { ThemeContext } from "../App";
+import axios from "axios";
+import SingleEnrollCourse from "./SingleEnrollCourse";
 const EnrolCourses = () => {
-    const {user} = useContext(ThemeContext)
-    const [courses, setCourses] = useState(null)
-    useEffect(()=>{
-        const config = {
-            headers: {
-              Authorization: `Bearer ${user.token}`,
-            },
-          };
-          axios
-            .get(
-              "http://localhost:5556/api/users/getenrollcourse",
-              config
-            )
-            .then((res) => {
-              if (res.data ) {
-                setCourses(res.data);
-                
-              } 
-            });
-    },[])
-    return (
+  const { user } = useContext(ThemeContext);
+  const [courses, setCourses] = useState(null);
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+    axios
+      .get("http://localhost:5556/api/users/getenrollcourse", config)
+      .then((res) => {
+        if (res.data) {
+          setCourses(res.data);
+        }
+      });
+  }, []);
+  return (
+    <div>
+      <h1 className="uppercase text-3xl font-semibold pb-2 text-five">
+        enrolled <span className=" text-[black]">courses</span>
+      </h1>
+      {courses && courses.length && (
         <div>
-            <h1 className='uppercase text-3xl font-semibold pb-2 text-five'>enrolled <span className=' text-[black]'>courses</span></h1>
-            {courses && 
-            <div>
-                {/* Single Course */}
-                {courses.map((e)=>{
-                    return <SingleEnrollCourse/>
-                })}
-                
-            </div> }
-            {!courses && 
-            <div className='bg-[#e9e4e4cb] py-10 rounded'>
-                <h1 className='text-3xl text-center'>No Courses Enrolled</h1>
-            </div> }
+          
+          {/* Single Course */
+          }
+          {courses.map((e) => {
+            // const date = e.createdAt.slice(0,10);
+            const date = new Date(e.createdAt).toDateString();
+            return (
+              <SingleEnrollCourse
+                key={e.course._id}
+                tittle={e.course.tittle}
+                img={e.course.img}
+                createdAt={date}
+              />
+            );
+          })}
         </div>
-    );
+      )}
+      {courses && !courses.length && (
+        <div className="bg-[#e9e4e4cb] py-10 rounded">
+          <h1 className="text-3xl text-center">No Courses Enrolled</h1>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default EnrolCourses;
--- frontend/src/User/SingleEnrollCourse.js
+++ frontend/src/User/SingleEnrollCourse.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const SingleEnrollCourse = (props) => {
-    return (
+  return (
+    <div>
+      <div className="grid grid-cols-3 gap-10 justify-center items-center p-5 hover:shadow-sm cursor-pointer rounded bg-[#ffffff]">
         <div>
-            <div className='grid grid-cols-3 gap-10 justify-center items-center p-5 hover:shadow-sm cursor-pointer rounded bg-[#ffffff]'>
-                    <div>
-                        <img className='w-50 h-50 rounded' src={props.img} alt="" />
-                    </div>
-                    <div>
-                        <h1 className='text-3xl font-semibold'>{props.tittle}</h1>
-                        <p className='py-5 text-xl'>Enrolled: {props.createdAt}</p>
-                    </div>
-                    <div>
-                        <button className='px-20 py-2 bg-[#36b323] hover:bg-[#2e9d1d] text-[white] rounded'>Add Review</button>
-                    </div>
-                </div>
+          <img className="w-50 h-50 rounded" src={props.img} alt="" />
         </div>
-    );
+        <div>
+          <h1 className="text-3xl font-semibold">{props.tittle}</h1>
+          <p className="py-5 text-xl">Enrolled: {props.createdAt}</p>
+        </div>
+        <div>
+          <button className="px-20 py-2 bg-[#36b323] hover:bg-[#2e9d1d] text-[white] rounded">
+            Add Review
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleEnrollCourse;
