28d126bf8875443790821a1c9a0288d0464d75b1
saimon hossain <saimonhossen05@saimons-MacBook-Air.local>
2022-12-28 16:38:47 +0600

    enrolled coursed added in profile

--- frontend/src/User/Profile.js
+++ frontend/src/User/Profile.js
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../App';
+import EnrolCourses from './EnrolCourses';
 
 const Profile = () => {
     const {user} = useContext(ThemeContext)
     return (
-        <div>
-            <h1 className='text-3xl uppercase text-center'>Welcome  <span className='text-[dodgerblue] font-bold'>{user.name}</span></h1>
+        <div className='my-10'>
+            <EnrolCourses/>
         </div>
     );
 };
