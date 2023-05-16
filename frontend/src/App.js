343ada50ed7e8283c74f1a35b8c8fa6035326c0e
aytullahdev <ayt.ullah.dev@gmail.com>
2023-05-16 21:51:45 +0600

    removime warning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import PageNotFound from "./errorhandle/PageNotFound";
 import Catagories from "./home/Catagories";
@@ -7,9 +7,8 @@ import Statussection from "./home/Statussection";
 import Login from "./User/Login";
 import "react-toastify/dist/ReactToastify.css";
 import Registration from "./User/Registration";
-import useUserstate from "./hooks/useUserstate";
 import Profile from "./User/Profile";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import SecureUser from "./User/SecureUser";
 import Contentupload from "./admin/Contentupload";
 import ReviewSlider from "./home/ReviewSlider";
