54f1a110ca9d80170aff9a12acda678dae5fe6a7
Syed-Mohiuzzaman24 <syedmohiuzzaman249@gmail.com>
2023-05-20 01:03:13 +0600

    footerupdated

--- frontend/src/home/Footer.js
+++ frontend/src/home/Footer.js
@@ -3,10 +3,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import ReactDOM from "react-dom";
 import { faGithub, faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="bg-[black]">
+      <Link to="/">
+        <div className="flex justify-center py-2">
+          <p className="text-4xl text-center lg:text-left font-bold text-white">
+            Lear<span className="text-five">ner</span>
+          </p>
+        </div>
+      </Link>
       <div className="text-center pt-5">
         <a href="">
           <FontAwesomeIcon
@@ -14,7 +22,7 @@ const Footer = () => {
             icon={faFacebook}
           />
         </a>
-        <a href="https://github.com/aytullahdev/learning">
+        <a href="https://github.com/ArthiDa/Learner_eLearning_Client">
           <FontAwesomeIcon
             className="text-center font-bold text-[blue] text-5xl mx-5 ring ring-[blue] rounded-full bg-[white] socialIcon"
             icon={faGithub}
